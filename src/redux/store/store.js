e49c2bdd2a723e0722e5f95f58d3b5a1f069088c
NghiNDP <ng.d.phuongnghi@gmail.com>
2022-03-31 11:33:55 +0700

    remove rubbish dir

--- src/redux/store/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import loadingReducer from '../reducers/loadingReducer'
-
-const store = configureStore({ reducer: loadingReducer })
-
-console.log(store.getState())
